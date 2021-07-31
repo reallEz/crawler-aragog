fbcc45bb602910b345bfbbd1cce56682d015ddaf
reallEz <jingqing.xj@outlook.com>
2021-07-31 13:56:19 +0800

    fix: call tread.start instead of run & force close imutable error report

--- src/main/java/com/github/reallEz/CrawlerAragog.java
+++ src/main/java/com/github/reallEz/CrawlerAragog.java
@@ -1,5 +1,6 @@
 package com.github.reallEz;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -18,6 +19,7 @@ import java.util.stream.Collectors;
 public class CrawlerAragog extends Thread {
     private final CrawlerAragogDao dao;
 
+    @SuppressFBWarnings("EI_EXPOSE_REP2")
     public CrawlerAragog(CrawlerAragogDao dao) {
         this.dao = dao;
     }
--- src/main/java/com/github/reallEz/Main.java
+++ src/main/java/com/github/reallEz/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         CrawlerAragogDao dao = new MybatisCrawlerAragogDao();
         for (int i = 0; i < 9; i++) {
-            new CrawlerAragog(dao).run();
+            new CrawlerAragog(dao).start();
         }
     }
 }
